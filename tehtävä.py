62a8474a99180353109e69fa83ca9612496a4b24
Jukka Prami <77621938+jukkaprami@users.noreply.github.com>
2023-03-22 16:23:39 +0200

    1

--- tehtävä.py
+++ tehtävä.py
@@ -1,7 +1,9 @@
-# Mitä koodi tulostaa?
-
-pineapples = 5
-zebras = 2
-
-print ((pineapples == 3) and (zebras == 2))
-print ((pineapples== 3) or (zebras == 2))
+print(max (10, 16, 30, 21, 25, 28))
+print(min(10, 16, 30, 21, 25, 28))
+print ('bang'.upper ())
+message = 'python on minusta kiva hieno'
+print(message.replace ('hieno', ':D'))
+print('python on minusta :D')
+countdown = [1, 2, 3]
+countdown.reverse ()
+print (countdown)
